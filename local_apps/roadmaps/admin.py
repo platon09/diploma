66471a3de91a76c2d30b3bb4cd7adc8802f2ef33
platon09 <nis.platon@gmail.com>
2022-03-13 00:00:32 +0600

    added models to admin panel

--- local_apps/roadmaps/admin.py
+++ local_apps/roadmaps/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.admin.filters import RelatedOnlyFieldListFilter
+from .models import Specialization, Section, Topic, Subtopic, MaterialType
 
-# Register your models here.
+
+@admin.register(Specialization)
+class SpecializationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = (('specialization', RelatedOnlyFieldListFilter),)
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = (('section', RelatedOnlyFieldListFilter),)
+
+
+@admin.register(Subtopic)
+class SubtopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'link')
+    list_filter = (('type', RelatedOnlyFieldListFilter),)
+
+
+@admin.register(MaterialType)
+class MaterialTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
