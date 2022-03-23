de5f179ed2702af4163b2cae7121194b415e4711
platon09 <nis.platon@gmail.com>
2022-03-23 10:36:10 +0600

    config core urls added api docs config admin page

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,17 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.utils.translation import gettext_lazy as _
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('local_apps.users.urls')),
+    path('api/roadmaps/', include('local_apps.roadmaps.urls')),
+    path('api/docs/', include_docs_urls(
+        title='API documentation'
+    )),
 ]
+
+admin.site.site_header = _("IT Bilim Administration")
+admin.site.site_title = _("IT Bilim Admin Portal")
+admin.site.index_title = _("Welcome to IT Bilim Portal")
