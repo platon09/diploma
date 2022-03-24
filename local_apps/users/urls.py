6a835e3e7384f057375c4ab1c9baa383227c07d0
platon09 <nis.platon@gmail.com>
2022-03-24 23:46:28 +0600

    added url for register

--- local_apps/users/urls.py
+++ local_apps/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
+from .api.views.register_view import RegisterView
 
 urlpatterns = [
+    path('register/', RegisterView.as_view()),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
