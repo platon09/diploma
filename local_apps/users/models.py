038d0636a11e2afa2073e78ccc7da7f117775a8f
platon09 <nis.platon@gmail.com>
2022-03-25 12:59:38 +0600

    create custom user model by AbstractBaseUser

--- local_apps/users/models.py
+++ local_apps/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class Customer(AbstractBaseUser):
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
