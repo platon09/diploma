07d3efc1cbd8c75658608d745229244f600ac551
platon09 <nis.platon@gmail.com>
2022-03-25 16:54:50 +0600

    bugfix: changed retrieveapiview to apiview, because retrieveapiview needs pk

--- local_apps/users/api/views/user_info_view.py
+++ local_apps/users/api/views/user_info_view.py
@@ -1,10 +1,13 @@
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from local_apps.users.models import Customer
 from local_apps.users.api.serializers.register_serializer import UserSerializer
+from rest_framework.response import Response
 
 
-class UserInfoView(RetrieveAPIView):
+class UserInfoView(APIView):
     permission_classes = (IsAuthenticated,)
-    queryset = Customer.objects.all()
-    serializer_class = UserSerializer
+
+    def get(self, request):
+        user = request.user
+        user_serializer = UserSerializer(user)
+        return Response(user_serializer.data)
