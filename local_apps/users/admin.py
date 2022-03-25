71129081eda67c2858fb1f0173f7de1694f602a1
platon09 <nis.platon@gmail.com>
2022-03-25 15:46:30 +0600

    Кастомер моделді админге қостым, іріктеу, сұрыптау методтарын қостым, өзгертуге болмайтын филдтарды ридонли қылдым

--- local_apps/users/admin.py
+++ local_apps/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    date_hierarchy = 'last_login'
+    list_display = ('email', 'full_name', 'last_login', 'is_staff', 'is_active',)
+    readonly_fields = ('password', 'email', 'date_joined',)
+    search_fields = ('email', 'first_name', 'last_login',)
+    list_filter = ('is_staff', 'is_active',)
