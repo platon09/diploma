c0bc8cc3ca4eaff2f9622da90fc4e7145917c2cd
platon09 <nis.platon@gmail.com>
2022-03-25 13:02:44 +0600

    changed user model to customer model

--- local_apps/users/api/serializers/register_serializer.py
+++ local_apps/users/api/serializers/register_serializer.py
@@ -1,24 +1,23 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from local_apps.users.models import Customer
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ('id', 'username', 'password', 'email', 'username', 'first_name', 'last_name')
+        model = Customer
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'],
-                                        password=validated_data['password'],
-                                        email=validated_data['email'],
-                                        first_name=validated_data['first_name'],
-                                        last_name=validated_data['last_name'])
+        user = Customer.objects.create_user(password=validated_data['password'],
+                                            email=validated_data['email'],
+                                            first_name=validated_data['first_name'],
+                                            last_name=validated_data['last_name'])
         return user
 
 
 # User serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = Customer
         fields = '__all__'
