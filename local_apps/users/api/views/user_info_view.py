0ac43685550cc2a164c3c2f249cd2d68e17ba178
platon09 <nis.platon@gmail.com>
2022-03-25 16:49:56 +0600

    added permission_classes

--- local_apps/users/api/views/user_info_view.py
+++ local_apps/users/api/views/user_info_view.py
@@ -1,7 +1,10 @@
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 from local_apps.users.models import Customer
 from local_apps.users.api.serializers.register_serializer import UserSerializer
 
+
 class UserInfoView(RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Customer.objects.all()
     serializer_class = UserSerializer
