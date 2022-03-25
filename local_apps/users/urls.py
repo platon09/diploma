f761ca065bb4d869918bba3b8f0dc3c404a7e94c
platon09 <nis.platon@gmail.com>
2022-03-25 16:40:55 +0600

    added endpoint for get user info

--- local_apps/users/urls.py
+++ local_apps/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 from .api.views.register_view import RegisterView
+from .api.views.user_info_view import UserInfoView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('me/', UserInfoView.as_view())
 ]
