6d79ab80b0b6fcde812aa59344229339da2503e8
platon09 <nis.platon@gmail.com>
2022-03-27 02:57:08 +0600

    created endpoints (urls)

--- local_apps/roadmaps/urls.py
+++ local_apps/roadmaps/urls.py
@@ -1,3 +1,21 @@
 from django.urls import path, include
+from local_apps.roadmaps.api.views.specialization_view import SpecializationListView, SpecializationDetailView
+from local_apps.roadmaps.api.views.section_view import SectionListView
+from local_apps.roadmaps.api.views.topic_view import TopicListView, TopicDetailView
+from local_apps.roadmaps.api.views.subtopic_view import SubTopicListView
 
-urlpatterns = []
+
+urlpatterns = [
+    path('', SpecializationListView.as_view()),
+    path('<slug:spec_slug>/', include([
+        path('', SpecializationDetailView.as_view()),
+        path('sections/', SectionListView.as_view()),
+        path('sections/<slug:section_slug>/', include([
+            path('', TopicListView.as_view()),
+            path('<slug:topic_slug>/', include([
+                path('',  TopicDetailView.as_view()),
+                path('subtopics/', SubTopicListView.as_view())
+            ])),
+        ])),
+    ])),
+]
