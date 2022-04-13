abb916964c95d80dc98afb17d1f35bde51977a1a
platon09 <nis.platon@gmail.com>
2022-04-13 16:23:02 +0600

    registered Recommendation to admin panel

--- local_apps/recommendations/admin.py
+++ local_apps/recommendations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Recommendation
 
-# Register your models here.
+
+@admin.register(Recommendation)
+class RecommendationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_on', 'image_tag')
