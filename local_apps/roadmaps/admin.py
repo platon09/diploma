c767cbe0872a55897253c5d0e3ed65a509d66a5f
platon09 <nis.platon@gmail.com>
2022-04-19 19:11:32 +0600

    refactored admin panels for roadmap models

--- local_apps/roadmaps/admin.py
+++ local_apps/roadmaps/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin.filters import RelatedOnlyFieldListFilter
-from .models import Specialization, Section, Topic, Subtopic, MaterialType
+from .models import Specialization, Techstack, Technology, Topic, Subtopic, MaterialType
 
 
 @admin.register(Specialization)
@@ -8,23 +8,28 @@ class SpecializationAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 
-@admin.register(Section)
-class SectionAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    list_filter = (('specialization', RelatedOnlyFieldListFilter),)
+@admin.register(Techstack)
+class TechstackAdmin(admin.ModelAdmin):
+    list_display = ('techstack_name', 'specialization')
+    list_filter = (('specialization', RelatedOnlyFieldListFilter), ('technology', RelatedOnlyFieldListFilter))
+
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'skills')
+    list_filter = (('skill', RelatedOnlyFieldListFilter),)
 
 
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    list_filter = (('section', RelatedOnlyFieldListFilter),)
+    list_display = ('name', 'technology')
+    list_filter = (('technology', RelatedOnlyFieldListFilter),)
 
 
 @admin.register(Subtopic)
 class SubtopicAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'link')
-    list_filter = (('type', RelatedOnlyFieldListFilter),)
-
+    list_display = ('name', 'type')
+    list_filter = (('type', RelatedOnlyFieldListFilter), ('topics', RelatedOnlyFieldListFilter))
 
 @admin.register(MaterialType)
 class MaterialTypeAdmin(admin.ModelAdmin):
