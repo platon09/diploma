c8b0b59979af7e4648aead4a166173c928aaf9e8
platon09 <nis.platon@gmail.com>
2022-04-19 19:14:44 +0600

    refactored topic_serializer.py

--- local_apps/roadmaps/api/serializers/topic_serializer.py
+++ local_apps/roadmaps/api/serializers/topic_serializer.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
-from local_apps.roadmaps.models import Topic
+from local_apps.roadmaps.models import Topic, Subtopic
+
+
+class SubTopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subtopic
+        fields = ['name', 'link', 'type']
+
+
+class TopicDetailSerializer(serializers.ModelSerializer):
+    subtopic = serializers.SerializerMethodField()
+
+    def get_subtopic(self, obj):
+        out = []
+        subtopics = Subtopic.objects.filter(topics__slug=obj.slug)
+        for item in subtopics:
+            out.append(SubTopicSerializer(item).data)
+        return out
+
+    class Meta:
+        model = Topic
+        fields = ['id', 'name', 'slug', 'description', 'is_learned', 'subtopic']
 
 
 class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
-        fields = ['id', 'name', 'slug', 'description']
+        fields = ['id', 'name', 'slug', 'description', 'is_learned']
