e0532a1df4124fa5d779749b82fc23031f3b4c3e
platon09 <nis.platon@gmail.com>
2022-04-19 19:16:38 +0600

    deleted

--- local_apps/roadmaps/api/serializers/section_serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from local_apps.roadmaps.models import Section
-
-
-class SectionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Section
-        fields = ['id', 'name', 'slug']
