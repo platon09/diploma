36199dacc95193912ab1ee363f8657835aab4215
platon09 <nis.platon@gmail.com>
2022-04-19 19:16:19 +0600

    added date_hierarchy/list_filter to RecommendationAdmin

--- local_apps/recommendations/admin.py
+++ local_apps/recommendations/admin.py
@@ -4,4 +4,6 @@ from .models import Recommendation
 
 @admin.register(Recommendation)
 class RecommendationAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_on'
     list_display = ('title', 'created_on', 'image_tag')
+    list_filter = ('created_on',)
