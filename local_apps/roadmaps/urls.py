212ffb0046f4118f0ed076094018acb2224b8819
platon09 <nis.platon@gmail.com>
2022-04-19 19:13:18 +0600

    refactored urls roadmaps app

--- local_apps/roadmaps/urls.py
+++ local_apps/roadmaps/urls.py
@@ -1,21 +1,23 @@
 from django.urls import path, include
-from local_apps.roadmaps.api.views.specialization_view import SpecializationListView, SpecializationDetailView
-from local_apps.roadmaps.api.views.section_view import SectionListView
+from local_apps.roadmaps.api.views.specialization_view import SpecializationListView
+from local_apps.roadmaps.api.views.techstack_view import TechstackListView
+from local_apps.roadmaps.api.views.technology_view import TechnologyListView, RecordProgressView
 from local_apps.roadmaps.api.views.topic_view import TopicListView, TopicDetailView
-from local_apps.roadmaps.api.views.subtopic_view import SubTopicListView
 
 
 urlpatterns = [
     path('', SpecializationListView.as_view()),
     path('<slug:spec_slug>/', include([
-        path('', SpecializationDetailView.as_view()),
-        path('sections/', SectionListView.as_view()),
-        path('sections/<slug:section_slug>/', include([
-            path('', TopicListView.as_view()),
-            path('<slug:topic_slug>/', include([
-                path('',  TopicDetailView.as_view()),
-                path('subtopics/', SubTopicListView.as_view())
-            ])),
+        path('', TechstackListView.as_view()),
+        path('<int:pk>/', include([
+            path('', TechnologyListView.as_view()),
+            path('<slug:tech_slug>/', include([
+                path('', TopicListView.as_view()),
+                path('done/', RecordProgressView.as_view()),
+                path('<slug:topic_slug>/', include([
+                    path('', TopicDetailView.as_view())
+                ])),
+            ]))
         ])),
     ])),
 ]
