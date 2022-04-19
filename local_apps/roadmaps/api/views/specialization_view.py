5119e51093eee539fdfc9e1440a2c6be2bfcc003
platon09 <nis.platon@gmail.com>
2022-04-19 19:15:05 +0600

    deleted SpecializationDetailView

--- local_apps/roadmaps/api/views/specialization_view.py
+++ local_apps/roadmaps/api/views/specialization_view.py
@@ -1,6 +1,4 @@
-from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 
 from local_apps.roadmaps.models import Specialization
 from local_apps.roadmaps.api.serializers.specialization_serializer import SpecializationSerializer
@@ -9,20 +7,3 @@ from local_apps.roadmaps.api.serializers.specialization_serializer import Specia
 class SpecializationListView(ListAPIView):
     queryset = Specialization.objects.all()
     serializer_class = SpecializationSerializer
-
-
-class SpecializationDetailView(RetrieveAPIView):
-    queryset = Specialization.objects.all()
-    serializer_class = SpecializationSerializer
-
-    def get_object(self):
-        specialization = Specialization.objects.get(slug=self.kwargs['spec_slug'])
-        return specialization
-
-    def retrieve(self, request, *args, **kwargs):
-        specialization = Specialization.objects.filter(slug=self.kwargs['spec_slug'])
-        if not specialization:
-            raise ValidationError(detail='Specialization is not found', code=400)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
