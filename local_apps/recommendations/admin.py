d52b5b8f19abd4927537dfa1f5f2787d874a1ca8
platon09 <nis.platon@gmail.com>
2022-04-24 14:27:28 +0600

    fix

--- local_apps/recommendations/admin.py
+++ local_apps/recommendations/admin.py
@@ -5,5 +5,5 @@ from .models import Recommendation
 @admin.register(Recommendation)
 class RecommendationAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_on'
-    list_display = ('title', 'created_on', 'image_tag')
+    list_display = ('title', 'created_on')
     list_filter = ('created_on',)
