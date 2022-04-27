ab0962a18c9eb10e4fe4e3ad32497722b5c23a55
platon09 <nis.platon@gmail.com>
2022-04-27 20:28:31 +0600

    fix study progress

--- local_apps/roadmaps/api/views/technology_view.py
+++ local_apps/roadmaps/api/views/technology_view.py
@@ -5,7 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from local_apps.roadmaps.models import Specialization, Topic, UserStudy
+from local_apps.users.models import Customer
+from local_apps.roadmaps.models import Specialization, Technology, UserStudy
 from local_apps.roadmaps.api.serializers.technology_serializer import TechnologySerializer
 
 
@@ -13,7 +14,7 @@ class TechnologyListView(ListAPIView):
     serializer_class = TechnologySerializer
 
     def get_queryset(self):
-        pk = self.kwargs['id']
+        pk = self.kwargs['spec_id']
         technology_qs = Specialization.objects.get(id=pk).technologies.all()
         return technology_qs
 
@@ -24,10 +25,11 @@ class RecordProgressView(APIView):
     def get(self, request, **kwargs):
         is_done = json.loads(request.query_params.get('done'))
         customer = request.user
-        tech_slug = kwargs['tech_slug']
+        tech_id = kwargs['tech_id']
 
-        userstudy = UserStudy.objects.get_or_create(user=customer, technology__slug=tech_slug)
-        num_topics = Topic.objects.filter(technology__slug=tech_slug).count()
+        tech = Technology.objects.get(id=tech_id)
+        userstudy = UserStudy.objects.get_or_create(user=customer, technology=tech)[0]
+        num_topics = tech.topics.count()
         percent = 100 / num_topics
 
         if is_done:
@@ -35,8 +37,11 @@ class RecordProgressView(APIView):
                 userstudy.progress += percent
                 userstudy.save()
                 userstudy_progress = userstudy.progress
-                if userstudy_progress != 100:
+                if userstudy_progress < 100:
                     return Response(f"Progress of {request.user.full_name} recorded: {round(userstudy.progress)}%. Added +{round(percent)}% to progress.")
+                elif userstudy_progress >= 100:
+                    customer.skill.add(*tech.skill.all().values_list('id', flat=True))
+                    return Response(f"Congratulations, you've learned enough about {userstudy.technology.name} technology!")
         else:
             if userstudy.progress > 0:
                 userstudy.progress -= percent
