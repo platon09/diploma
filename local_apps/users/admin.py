df34ff9df5c37f4d671e211df0ad6f487aef5586
platon09 <nis.platon@gmail.com>
2022-04-28 09:54:52 +0600

    added logger for actions in admin

--- local_apps/users/admin.py
+++ local_apps/users/admin.py
@@ -1,5 +1,48 @@
 from django.contrib import admin
+from django.contrib.admin.filters import RelatedOnlyFieldListFilter
+from django.utils.html import format_html
+from config.settings import BACKEND_URL
+
+from django.contrib.admin.models import LogEntry
 from .models import Skill, Customer
+
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'action_time'
+    exclude = ['object_repr', 'change_message']
+    readonly_fields = ('action_time', 'user', 'action_flag', 'object_id', '__str__',
+                       'link', 'content_type',)
+    ordering = ('-action_time',)
+    search_fields = ('content_type', '__str__', 'user')
+    list_display = ('action_time', 'user', 'action_flag', '__str__', 'link',)
+    list_filter = ('action_flag', ('user', RelatedOnlyFieldListFilter),
+                   ('content_type', RelatedOnlyFieldListFilter))
+
+    def get_queryset(self, request):
+        queryset = super(LogEntryAdmin, self).get_queryset(request)
+        if request.resolver_match.func.__name__ not in ['change_view', 'add_view']:
+            qs = queryset.select_related('content_type', 'user')
+        else:
+            qs = queryset
+        return qs
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    @staticmethod
+    def link(obj):
+        admin_url = obj.get_admin_url()
+        if admin_url:
+            url = BACKEND_URL + admin_url
+            return format_html('<a href="{}">{}</a>', url, 'link to '+obj.object_repr)
+        return "null"
 
 
 @admin.register(Customer)
