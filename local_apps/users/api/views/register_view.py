5086b57fa1c68c07e1306dd798f82136b0686071
platon09 <nis.platon@gmail.com>
2022-06-15 16:13:27 +0600

    added permission to register request

--- local_apps/users/api/views/register_view.py
+++ local_apps/users/api/views/register_view.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from local_apps.users.api.serializers.register_serializer import RegisterSerializer, UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -6,6 +7,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 class RegisterView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
 
     def post(self, request, *args,  **kwargs):
         serializer = self.get_serializer(data=request.data)
